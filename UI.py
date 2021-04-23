a6836ee4eefbb61977e1957554538bc0520145e3
RasikKane <rasik.kane@gmail.com>
2021-04-23 13:55:08 +0100

    UI connected with MultiDirectory module

--- UI.py
+++ UI.py
@@ -5,6 +5,8 @@ Tkinter UI for application creating multi-folder structure
 from tkinter import *
 from tkinter import filedialog
 
+import multiDirectory as mDir
+
 
 class App:
 
@@ -18,16 +20,19 @@ class App:
         frame = Frame(master)
         frame.pack()
 
-        self.file_button = Button(frame, text="input file")
+        self.file_button = Button(frame, text="input file",
+                                  command=lambda: mDir.browse_button(self.file, self.open_file))
         self.file_button.pack(side=LEFT)
 
-        self.dir_button = Button(frame, text="destination folder", fg="red")
+        self.dir_button = Button(frame, text="destination folder", fg="red",
+                                 command=lambda: mDir.browse_button(self.directory, self.open_dir))
         self.dir_button.pack(side=LEFT)
 
-        self.enum_radio = Radiobutton(root, text="Enumerate", variable=self.flag, value=True)
+        self.enum_radio = Radiobutton(root, text="Enumerate", variable=self.flag, value=True,
+                                      command=lambda: mDir.set_flag(self.flag.get()))
         self.enum_radio.pack(side=LEFT)
 
-        self.make_dir = Button(frame, text="make directories")
+        self.make_dir = Button(frame, text="make directories", command=mDir.driver)
         self.make_dir.pack(side=LEFT)
 
 
